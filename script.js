37821e54a9adf1cac1c65181c0b6051a84923dd8
urie-w <urieasia@gmail.com>
2024-07-19 21:52:42 -0600

    update new city still some bugs

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Get Api
-const url = 'https://open-weather13.p.rapidapi.com/city/landon/EN';
+const url = 'https://open-weather13.p.rapidapi.com';
 const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': '8bf6ed7b44msh01e68ae90efda05p1bb526jsn55a396da0415',
-		'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': '8bf6ed7b44msh01e68ae90efda05p1bb526jsn55a396da0415',
+        'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
+    }
 };
 
 // Responses to different results
@@ -17,7 +17,7 @@ async function fetchData() {
         }
         const result = await response.json();
 
-// Link data to html page
+        // Link data to html page
 
         document.getElementById('city').textContent = result.city;
         document.getElementById('state').textContent = result.state;
@@ -27,5 +27,38 @@ async function fetchData() {
         console.log(result);
     } catch (error) {
         console.error(error);
+    }
+}
+
+// Fetch data when page loads up
+fetchData();
+
+// Fetch data whn search button is clicked
+
+document.getElementById('search-btn').addEventListener('click', function () {
+    const citySearch = document.getElementById('search-input').value.trim();
+    if (citySearch === "") {
+        alert('Please enter a city');
+    } else {
+        const newUrl = `https://open-weather13.p.rapidapi.com/city/${citySearch}/EN`;
+        fetchData(newUrl);
+    }
+})
+
+async function fetchUpdatedData(newUrl) {
+    try {
+        const response = await fetch(newUrl, options);
+        if (!response.ok) {
+            throw new Error('Network response was not found');
+        }
+        result = await response.json();
+
+        document.getElementById('city').textContent = result.city;
+        document.getElementById('state').textContent = result.state;
+        document.getElementById('temperature').textContent = result.temperature;
+        document.getElementById('condition').textContent = result.condition;
+
+    } catch (error) {
+        console.error('There was an issue finding the weather data', error);
     }
 }
