205e285eaeeb3c462d47a2c9045b8f576fd21214
urie-w <urieasia@gmail.com>
2024-07-31 17:51:43 -0600

    Javascript started

--- script.js
+++ script.js
@@ -1,64 +1,22 @@
-// // Get Api
-// const url = 'https://open-weather13.p.rapidapi.com';
-// const options = {
-//     method: 'GET',
-//     headers: {
-//         'x-rapidapi-key': '8bf6ed7b44msh01e68ae90efda05p1bb526jsn55a396da0415',
-//         'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
-//     }
-// };
+const searchButoon = document.querySelector("button");
+const cityInput = document.querySelector("#city");
+const WeatherIcon = document.querySelector("#weather-icon");
+const temperature = document.querySelector("#temperature");
+const weatherInfo = document.querySelector("#weather-info");
+const hourlyWeather = document.querySelector("#hourly-weather");
+const forecast = document.querySelector("#forecast");
+const searchHistory = document.querySelector("#search-history");
+const API_Key = "8bf6ed7b44msh01e68ae90efda05p1bb526jsn55a396da0415";
+const API_Host = "worldapi.p.rapidapi.com";
 
-// // Responses to different results
-// async function fetchData() {
-//     try {
-//         const response = await fetch(url, options);
-//         if (!response.ok) {
-//             throw new Error('Network response was not found');
-//         }
-//         const result = await response.json();
-
-//         // Link data to html page
-
-//         document.getElementById('city').textContent = result.city;
-//         document.getElementById('state').textContent = result.state;
-//         document.getElementById('temperature').textContent = result.temperature;
-//         document.getElementById('condition').textContent = result.condition;
-
-//         console.log(result);
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
-
-// // Fetch data when page loads up
-// fetchData();
-
-// // Fetch data whn search button is clicked
-
-// document.getElementById('search-btn').addEventListener('click', function () {
-//     const citySearch = document.getElementById('search-input').value.trim();
-//     if (citySearch === "") {
-//         alert('Please enter a city');
-//     } else {
-//         const newUrl = `https://open-weather13.p.rapidapi.com/city/${citySearch}/EN`;
-//         fetchData(newUrl);
-//     }
-// })
-
-// async function fetchUpdatedData(newUrl) {
-//     try {
-//         const response = await fetch(newUrl, options);
-//         if (!response.ok) {
-//             throw new Error('Network response was not found');
-//         }
-//         result = await response.json();
-
-//         document.getElementById('city').textContent = result.city;
-//         document.getElementById('state').textContent = result.state;
-//         document.getElementById('temperature').textContent = result.temperature;
-//         document.getElementById('condition').textContent = result.condition;
-
-//     } catch (error) {
-//         console.error('There was an issue finding the weather data', error);
-//     }
-// }
+//elements for current weather
+const city = document.querySelector("#city");
+const temp = document.querySelector("#temp");
+const wind = document.querySelector("#wind");
+const humidity = document.querySelector("#humidity");
+const firstIcon = document.querySelector("#first-icon");
+const date = document.querySelector("#date");
+//Get weather data from API
+function getCurrentWeather () {
+    fetch(`https://rapidapi.com/worldapi/api/open-weather13/playground/apiendpoint_d15cd885-e8e5-49e7-b94b-588c41687aa1`);
+}
