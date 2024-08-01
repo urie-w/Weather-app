3c5261016438a181e41a5b2f2c016a4157ef0f67
urie-w <urieasia@gmail.com>
2024-07-31 18:30:20 -0600

    added event listeners and search history

--- script.js
+++ script.js
@@ -44,3 +44,82 @@ function getCurrentWeather () {
 };
 
 //Function to get forecast data from api
+function getForecast () {
+    fetch(`https://${API_Host}/forecast?city=${cityInput.value}`, {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': API_Key,
+            'X-RapidAPI-Host': API_Host
+        }
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log('Date for 5 day forecast');
+        console.log(data);
+    })
+    .catch(err => console.log(err));
+}
+// Select data for 5 day forecast
+const selectData = [
+    data.list[0],
+    data.list[8],
+    data.list[16],
+    data.list[24],
+    data.list[32],
+];
+
+console.log(selectData);
+
+// takes data from selectData and fills values in forecast cards
+for (let i = 0; i < selectData.length; i++) {
+   const date = document.querySelector(`#date${i}`);
+   const icon = document.querySelector(`#icon${i}`);
+   const temp = document.querySelector(`#temp${i}`);
+   const humidity = document.querySelector(`#humidity${i}`);
+   const wind = document.querySelector(`#wind${i}`);
+
+   date.textContent = dayjs(selectData[i].dt * 1000).format('MM/DD/YYYY');
+   const icon = document.createElement("img");
+   icon.innerHTML = "";
+   icon.appendChild(icon);
+   icon.alt = "Weather Icon";
+   icon.src = `http://openweathermap.org/img/w/${selectData[i].weather[0].icon}.png`;
+   temp.textContent = `Temp: ${selectData[i].main.temp}`;
+   humidity.textContent = `Humidity: ${selectData[i].main.humidity}`;
+   wind.textContent = `Wind: ${selectData[i].wind.speed}`;
+}
+
+//Event listener for search
+searchButton.addEventListener('Click', function () {
+    getCurrentWeather();
+    getForecast();
+});
+
+//Event listener for search history
+const cities = JSON.parse(localStorage.getItem('city')) || [];
+city.ariaReadOnly.push(cityInput.value);
+localStorage.setItem('city', JSON.stringify(city));
+loadSearchHistory();
+});
+
+//Display search history
+function loadSearchHistory () {
+ const savedCities = JSON.parse(localStorage.getItem('city')) || [];
+ searchHistory.innerHTML = "";
+
+ for (let i = 0; i < savedCities.length; i++) {
+    let citybtn = document.createElement("button");
+    citybtn.textContent = savedCities[i];
+    citybtn.addEventListener("click", function(event){
+        console.log(event.target.textContent);
+        cityInput.value = event.target.textContent;
+        getCurrentWeather();
+        getForecast();
+    });
+    searchHistory.appendChild(citybtn);
+    }
+}
+
+//load search history
+loadSearchHistory();
+
